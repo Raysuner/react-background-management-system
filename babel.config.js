e751851ccce25b7878c4da7642d5fbfcb6e3dedb
itnd <1669232805@qq.com>
2024-01-05 22:36:39 +0800

    chore: 增加babel配置文件注释

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: [
     "@babel/preset-env",
     "@babel/preset-typescript",
+    // 增加runtime: "automatic", 可以不再需要手动引入React
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
 };
