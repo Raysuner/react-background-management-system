33053fe88eb6d00599a90a4e67f3dd222427211d
itnd <1669232805@qq.com>
2024-01-06 22:03:40 +0800

    fix: 去除测试代码

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Forbidden403 from "../pages/Forbidden403";
 import NotFound404 from "../pages/NofFound404";
@@ -16,11 +16,11 @@ const router = [
   {
     path: "/404",
     element: <NotFound404 />
+  },
+  {
+    path: "*",
+    element: <Navigate to="/404" />
   }
-  // {
-  //   path: "*",
-  //   element: <Navigate to="/404" />
-  // }
 ];
 
 export default createBrowserRouter(router);
