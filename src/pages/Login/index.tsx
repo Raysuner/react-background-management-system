7236e71e2c779144930d4968256b2f0e8dbb289e
itnd <1669232805@qq.com>
2024-01-10 09:00:03 +0800

    feat: 增加登录页面

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,34 @@
 import "./index.scss";
 import LoginBgImage from "public/login_bg.jpeg";
+import { Input, Form, Button } from "antd";
 
 export default function Login() {
+  const [form] = Form.useForm();
   return (
     <div
       className="login-page-container"
       style={{ backgroundImage: `url(${LoginBgImage})` }}
-    ></div>
+    >
+      <div className="login-container">
+        <div className="title">请登录</div>
+        <Form
+          className="login-info"
+          form={form}
+          onFinish={(values) => {
+            console.log("values", values);
+          }}
+        >
+          <Form.Item name="username">
+            <Input placeholder="请输入账号" />
+          </Form.Item>
+          <Form.Item name="password">
+            <Input placeholder="请输入密码" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            登录
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
