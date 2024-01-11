94f3a6e4f0485aea3b338e4fba5fcd5ea7775294
itnd <1669232805@qq.com>
2024-01-11 21:15:16 +0800

    feat: 支持全局化配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import { RouterProvider } from "react-router-dom";
+import { ConfigProvider } from "antd";
 import router from "./router";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#ed6c00"
+        }
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
