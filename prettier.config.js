e8527a3259ac511c9a3c4c2567654a55b094faaf
Raysuner <1669232805@qq.com>
2025-06-21 17:06:15 +0800

    添加配置文件

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 单行最大长度
+  printWidth: 100,
+  // 缩进空格数
+  tabWidth: 2,
+  // 使用空格缩进
+  useTabs: false,
+  // 语句末尾使用分号
+  semi: true,
+  // 使用单引号
+  singleQuote: true,
+  // 最后一个对象属性加逗号
+  trailingComma: "all",
+  // 大括号内的首尾需要空格
+  bracketSpacing: true,
+  // jsx标签的反尖括号需要换行
+  bracketSameLine: false,
+  // 箭头函数参数括号
+  arrowParens: "always",
+};
